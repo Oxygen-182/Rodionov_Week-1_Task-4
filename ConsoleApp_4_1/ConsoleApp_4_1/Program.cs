13e22054e9d843fba43d50d1518f5463e7b027da
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-05 17:59:52 +0300

    Add files via upload

---
+++ ConsoleApp_4_1/ConsoleApp_4_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_4_1
+{
+    internal class Program
+    {
+        static int f(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + f(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите m: ");
+                    int m = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите k: ");
+                    int k = int.Parse(Console.ReadLine());
+
+                    if (m < 1 && k < 1) throw new Exception("Оба числа введены некорректно");
+                    if (m < 1) throw new Exception("Число m введено некорректно");
+                    if (k < 1) throw new Exception("Число k введено некорректно");
+
+                    int x = f(m) + f(2 * k);
+                    Console.WriteLine($"Выражение = {x}");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n\n");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp_4_1_form/ConsoleApp_4_1_form/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ConsoleApp_4_1_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static double f(double n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + f(n - 1);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+
+            double m = double.Parse(textBox1.Text);
+            double k = double.Parse(textBox2.Text);
+
+            if (m % 1 != 0) 
+            {
+                textBox3.Text += "Число m введено некорректно";
+                return;
+            }
+            if (k % 1 != 0)
+            {
+                textBox3.Text += "Число m введено некорректно";
+                return;
+            }
+            if (m < 1 && k < 1)
+            {
+                textBox3.Text += "Оба числа введены некорректно";
+                return;
+            } 
+            if (m < 1)
+            {
+                textBox3.Text += "Число m введено некорректно";
+                return;
+            }
+            if (k < 1)
+            {
+                textBox3.Text += "Число k введено некорректно";
+                return;
+            }
+
+            double x = f(m) + f(2 * k);
+
+            textBox3.Text += $"{x}";
+        }
+    }
+}
