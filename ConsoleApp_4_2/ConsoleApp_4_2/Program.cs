a0911dd15d25e31fdce893f43d2b9ddf83bd1659
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2022-12-08 09:44:23 +0300

    Add files via upload

---
+++ ConsoleApp_4_2/ConsoleApp_4_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_4_2
+{
+    internal class Program
+    {
+        static void f(int n)
+        {
+            if (n % 10 == n)
+            {
+                Console.Write(n + " ");
+            }
+            if (n / 10 == 0) return;
+            f(n / 10);
+            Console.Write(n % 10 + " ");
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите A: ");
+                    int a = int.Parse(Console.ReadLine());
+
+                    Console.Write("Введите B: ");
+                    int b = int.Parse(Console.ReadLine());
+
+                    if (a > b) throw new Exception("Число A не может быть больше B");
+                    if (a <= 0 && b <= 0) throw new Exception("Числа не могут иметь значения меньше или равные нулю");
+                    if (a <= 0) throw new Exception("Число A не может иметь значение меньше или равное нулю");
+                    if (b <= 0) throw new Exception("Число B не может иметь значение меньше или равное нулю");
+
+                    Console.WriteLine("\nЦифры натуральных чисел из интервала (AB) в прямом порядке:\n");
+
+                    for (int i = a; i <= b; i++)
+                    {
+                        f(i);
+                    }
+
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}\n\n");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp_4_2_form/ConsoleApp_4_2_form/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp_4_2_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void f(int n, TextBox textBox3)
+        {
+            if (n % 10 == n)
+            {
+                textBox3.Text += n + " ";
+            }
+            if (n / 10 == 0) return;
+            f(n / 10, textBox3);
+            textBox3.Text += (n % 10) + " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+
+            int a;
+            if (int.TryParse(textBox1.Text, out a) && a > 0)
+            {
+            }
+            else
+            {
+                textBox3.Text += "Число A введено некорректно";
+                return;
+            }
+
+            int b = int.Parse(textBox2.Text);
+            if (int.TryParse(textBox2.Text, out b) && b > 0)
+            {
+            }
+            else
+            {
+                textBox3.Text += "Число B введено некорректно";
+                return;
+            }
+
+            if (a > b)
+            {
+                textBox3.Text += "Число A не может быт больше B";
+                return;
+            }
+            if (a <= 0 && b <= 0)
+            {
+                textBox3.Text += "Числа не могут иметь значения меньше или равные нулю";
+                return;
+            }
+            if (a <= 0)
+            {
+                textBox3.Text += "Число A не может иметь значение меньше или равное нулю";
+                return;
+            }
+            if (b <= 0)
+            {
+                textBox3.Text += "Число B не может иметь значение меньше или равное нулю";
+                return;
+            }
+
+            textBox3.Text += "Цифры натуральных чисел из интервала (AB) в прямом порядке:" + Environment.NewLine;
+
+            for (int i = a; i <= b; i++)
+            {
+                f(i, textBox3);
+            }
+        }
+    }
+}
